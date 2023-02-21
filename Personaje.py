db59cd08240abfe5361221f5487540f96053d989
Akemi150 <akemi2002@hotmail.com>
2023-02-20 18:43:13 -0600

    Definimos métodos del personaje

--- Personaje.py
+++ Personaje.py
@@ -1,8 +1,24 @@
 
 class Personaje: 
+    
 #ATRIBUTOS DEL PERSONAJE
-especie = “Humano”
-nombre = “Marcus fenix”
+
+especie = "Humano"
+nombre = "Fenix"
 altura = 1.90
 
 #Metodos Personaje
+
+def correr(self, status):
+    if(status):
+        print("El personaje "+ self.nombre + "esta corriendo" )
+    else:
+        print("El personaje "+ self.nombre + "se detuvo" )    
+        
+def lanzarGranada (self):
+    print ("Se lanzo Granada ")   
+
+def recargarArma(self, munciones): 
+    cargador= 5
+    cargador = cargador + munciones     
+    print("El arma tiene ahora "+ cargador +"balas")
