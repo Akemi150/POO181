d196c905ca25b1d061b43acc0c46ac7ade287e4b
Akemi150 <akemi2002@hotmail.com>
2023-03-01 18:08:32 -0600

    FUNCIONES DE BOTONES

--- tkinther/Practica11.py
+++ tkinther/Practica11.py
@@ -1,6 +1,20 @@
 
-from tkinter import Tk,Frame,Button
+from tkinter import Tk,Frame,Button,messagebox
 
+#4.Función de mensajes para el botón
+
+def mostrarMensaje():
+    messagebox.showinfo("Aviso", "Este mensaje es para informar algo")
+    messagebox.showerror("Error:","Todo fallo con éxito")
+    print(messagebox.askquestion("Pregunta:","El o ella jugo con tu corazón"))
+
+#5. Función para agregar botones:
+def agregarBoton():
+    BotonAmarillo.config(text="+", bg="#cc9900")
+    BotonNuevo= Button(Seccion3,text="Boton Nuevo")
+    BotonNuevo.pack()
+    
+    
 #1. Crear ventana
 ventana= Tk()
 ventana.title(" Practica 11:Frames ")
@@ -17,7 +31,7 @@ Seccion3=Frame(ventana,bg="#ffffcc")
 Seccion3.pack(expand=True,fill='both')
 
 #3. Botones
-BotonAzul= Button(Seccion1, text="Botón Azul" ,fg="#000d1a")
+BotonAzul= Button(Seccion1, text="Botón Azul" ,fg="#000d1a",command=mostrarMensaje )
 BotonAzul.place(x=60, y=60)
 
 BotonRojo= Button(Seccion2, text="Botón Rojo" ,fg="#000d1a")
@@ -28,7 +42,7 @@ BotonNegro= Button(Seccion2, text="Botón Negro" ,fg="#000d1a", bg="#5c5c8a")
 BotonNegro.grid(row=1, column=0)
 BotonNegro.place(x=60, y=60)
 
-BotonAmarillo= Button(Seccion3, text="Botón Amarillo" ,bg="#e6e600")
+BotonAmarillo= Button(Seccion3, text="Botón Amarillo" ,bg="#e6e600",command= agregarBoton)
 BotonAmarillo.pack()
 
 
