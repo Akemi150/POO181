c8b423cc4579691f49728a4e4ae3d0caea4b255d
Akemi150 <akemi2002@hotmail.com>
2023-03-24 19:25:51 -0600

    Se hicieron las conexiones

---
+++ tkinterSQLite/controladorBD.py
@@ -0,0 +1,58 @@
+from tkinter import messagebox
+import sqlite3
+import bcrypt
+
+class controladorBD:
+    
+    def __init__(self):
+        pass
+    
+    # Metodo para crear conexiones
+    def conexionBD(self):
+        try:
+            conexion= sqlite3.connect("/Users/akemi150/Documents/GitHub/POO181/tkinterSQLite/DBUsuarios.db")
+            print("Conectado a la BD")
+            return conexion 
+        except sqlite3.OperationalError:
+            print("No se pudo conectar")
+    
+    #Metodo para Guardar Usuarios
+    def guardarUsuario(self,Nom,Cor,Con):
+        
+        #1. Usamos una conexion
+        Conx= self.conexionBD()
+        
+        #2. Validar parametros Vacios
+        if(Nom == "" or Cor == "" or Con == ""):
+            messagebox.showwarning("Aguas", "Formulario Incompleto")
+        else:
+            
+            #3. Preparamos Cursor,Datos,QuerySQL
+            Cursor= Conx.cursor()  
+            ConH= self.encriptarCon(Con)
+            Datos=(Nom,Cor,ConH)
+            qrInsert= "insert into TBRegistrados(nombre,correo,contra) values(?,?,?)" 
+            
+            #4. Ejecutar Insert y cerramos Conexión
+            Cursor.execute(qrInsert,Datos) 
+            Conx.commit()
+            Conx.close
+            messagebox.showinfo("Éxito", "Usuario Guardado")
+        
+        
+    # Método para encriptar Contraseñas    
+    def encriptarCon(self,Con):
+            ConPlana= Con
+            
+            
+            #Preparamos con en bytes y la SAL
+            ConPlana= ConPlana.encode()    #Convertimos con a bytes
+            Sal= bcrypt.gensalt()
+            
+            #Encryptamos la contraseña
+            ConHa= bcrypt.hashpw(ConPlana, Sal)
+            print(ConHa)
+            return ConHa
+        
+        
+        
